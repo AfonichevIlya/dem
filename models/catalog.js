b727fdd803d60a769c16da9b99f46070a0d5cd79
Afonichev Ilya <afonichev007@gmail.com>
2024-05-21 11:56:52 +0300

    commit

--- models/catalog.js
+++ models/catalog.js
@@ -1,41 +1,39 @@
 const Sequelize = require("sequelize");
 
 module.exports = (sequelizeMethod) => {
-    const Catalog = sequelizeMethod.define(
-        "Catalog",
-        {
-            id: {
-                type: Sequelize.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-            },
-            name: {
-                type: Sequelize.STRING(100),
-                allowNull: false,
-            },
-            category: {
-                type: Sequelize.STRING(100),
-                allowNull: false,
-            },
-            img_url: {
-                type: Sequelize.STRING(100),
-                allowNull: false,
-            },
-            price: {
-                type: Sequelize.STRING(100),
-                allowNull: false,
-            },
-            description: {
-                type: Sequelize.STRING,
-                allowNull: false,
-            },
+  const Catalog = sequelizeMethod.define(
+    "Catalog",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      category: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      img_url: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
 
-            toggle: {
-                type: Sequelize.BOOLEAN,
-                defaultValue: true,
-            },
-        },
-        { timestamps: true }
-    );
-    return Catalog;
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      toggle: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+      },
+    },
+    { timestamps: true }
+  );
+
+  return Catalog;
 };
